7f801927ceb9fbe6f658531e3636eb626bfcb574
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-21 12:10:27 +0000

    operators usage double == and tripe ===

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,35 @@
+// console.log(2 > 1)
+// console.log(2 == 1)
+// console.log(2 <= 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+
+
+// console.log("2" > 1)
+// console.log(2 < "11")
+// console.log("11a" > 3)
+
+// console.log(null < 0)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null <= 0)
+// The reason for the above is because == and < > <= >= work differently in
+// Javascript as in, all other operators except == convert null to 0 for 
+// comparison
+
+// console.log(true > 89)
+// console.log(true)
+// console.log(String(true));
+
+// console.log(undefined < 0)
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+// console.log(undefined <= 0)
+
+
+console.log("2" === 2)
+
+
+
