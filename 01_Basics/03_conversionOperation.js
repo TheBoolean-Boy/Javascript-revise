ae5ffde5e4bf27bfed36371f83f1b4931c3a6e6e
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-21 11:46:27 +0000

    Operations involving comparisons

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -28,8 +28,41 @@ let score = false
 let random = undefined
 
 let convert2String = String(random)
-console.log(convert2String);
-console.log(typeof convert2String);
+// console.log(convert2String);
+// console.log(typeof convert2String);
 
 //Evrything => String
+
+//----------------------Operations-------------
+
+let str1 = "Saswat"
+let str2 = " Rath"
+
+let str3 = str1 + str2
+
+// console.log(str3);
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("2" + 1 +  1)
+// console.log(1 + 1 + "2")
+
+// console.log("4" / "2" + "2")
+// console.log("4" % 2 + "6")
+// console.log("2" + "4" + "2")
+// console.log("6" / "2" * "4")
+// console.log("2" + "9" % "6")
+// console.log(2 + "2" + "6" / 3 + "4" / 2 + "2")
+// console.log("2" + 1 + 2)
+// console.log("2"+  1 % "2");
+
+
+// console.log(+true)
+// console.log(true+) <-- throws an error
+
+// console.log(+"")
+
+let counter = 199;
+console.log(++counter)
+
 
