fe5514933a8a21eb22005d1b8b8f3f4f7cf828dd
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-21 08:25:30 +0000

    conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+let score = false
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let convertToNumber = Number(score);
+// console.log(convertToNumber);
+
+// 33 => 33
+// 33abs => NaN
+// saswat => NaN
+// true => 1 , false => 0
+// null => 0
+// undefined => NaN
+
+// let isLoggedIn = ""
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+// console.log(typeof booleanIsLoggedIn)
+
+// 1 => true, 2=> true....so on
+// -1 => true, -2 -> true.... so on
+// (Empty string)"" => false
+// "rt56" => true
+// 0, undefined, null => false
+
+let random = undefined
+
+let convert2String = String(random)
+console.log(convert2String);
+console.log(typeof convert2String);
+
+//Evrything => String
+
