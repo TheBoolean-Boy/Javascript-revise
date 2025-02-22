73f1776b52469a7f9ebd10eed1ce97e77ed3dff5
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-22 07:57:28 +0000

    Numbers and their ascoiated inbuilt functions

---
+++ 01_Basics/06_numbersandMaths.js
@@ -0,0 +1,18 @@
+const score = 400
+
+const you = new Number(100.78996)
+// console.log(typeof you.toFixed(3))
+// console.log(typeof you)
+
+// console.log(you.toString().substring(0,5))
+
+const yours = 100.78
+// console.log(yours.toPrecision(6))
+// console.log(typeof yours.toPrecision(6))
+
+const hundreds = 100423423423
+// console.log(hundreds.toLocaleString('en-IN'));
+// console.log(typeof hundreds.toLocaleString('en-IN'));
+
+
+// ----------------------Maths------------------------------
