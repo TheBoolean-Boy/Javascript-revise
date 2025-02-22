7d7e6fb22ea050f151d51096682dc8bb527353b4
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-22 09:16:09 +0000

    mATHS IN jAVASCRIPT

--- 01_Basics/06_numbersandMaths.js
+++ 01_Basics/06_numbersandMaths.js
@@ -16,3 +16,23 @@ const hundreds = 100423423423
 
 
 // ----------------------Maths------------------------------
+// const arr = [4,  8, 9, 90]
+
+// console.log(Math.floor(7.8))
+// console.log(Math.ceil(7.8));
+// console.log(Math.round(4.1));
+// console.log(Math.round(4.6));
+// console.log(Math.abs(-9))
+// console.log(Math.max(4,  8, 9, 90))
+// console.log(Math.min(4,  8, 9, 90));
+
+// console.log(Math.random())
+// console.log(Math.random()*10000)
+
+//---------Generate a random number withnin a given range---------
+const min = 6;
+const max = 11;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+
+// The above works because (max - min + 1) is used because we wanna multiply the size of our range and then add the min no. to it. Let's rmage is 7 to 12. 7 + anything between the 0 and size of the of range 
