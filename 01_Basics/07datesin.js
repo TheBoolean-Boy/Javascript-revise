0ea921d7aeea8a661b4a51e92d93b56ae1784b3e
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-22 16:19:50 +0000

    Proceeding further

---
+++ 01_Basics/07datesin.js
@@ -0,0 +1,9 @@
+let myDate = new Date()
+
+// console.log(myDate.toString())
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+
+let myownDate = new Date(2025,  0, 31, 14, 56)
+console.log(myownDate.toLocaleString())
---
+++ 02_Basics/01_Arrays.js
@@ -0,0 +1,37 @@
+ 
+
+ const arr = [ 0, 1, 2, 3, 4, 5]
+ const arr2= ["Hello", "Hi", "834328"]
+
+//  console.log(arr2[2], arr[3])
+
+ const myArr = new Array(1, "Sup", "append", 98, 56, "saswat", 3)
+
+//  console.log(myArr.splice(2, 3))
+ 
+// myArr.push(1)
+// myArr.push(9909)
+// myArr.pop()
+
+// myArr.unshift(3)
+// myArr.shift()
+
+// console.log(myArr.includes(82384))
+// console.log(myArr.indexOf(1))
+
+
+const newArr = myArr.join()
+// console.log(newArr.toUpperCase())
+// console.log(typeof myArr)
+
+// console.log(myArr)
+
+const trialArr = new Array(1, 2, 3, 4, 5)
+ 
+
+// console.log(trialArr.slice(1,4));
+// console.log(trialArr);
+
+// console.log(trialArr.splice(1, 4));
+// console.log(trialArr)
+
