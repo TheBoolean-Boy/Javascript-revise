e11a74391836a5d6ce689154c7deef78dc263414
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-23 12:07:39 +0000

    expression funcition

--- 03_Basics/02_scopes.js
+++ 03_Basics/02_scopes.js
@@ -6,9 +6,19 @@ if(true){
     let a  = 34;
     const b = 56;
     var c =  67;
-    console.log(a, b, c);
+    // console.log(a, b, c);
 }
-console.log(a, b, c)
+// console.log(a, b, c)
 
 //var violates scopes
 //local and global scopes
+
+console.log(addOne(4));
+function addOne(num){
+    return num + 1;
+}
+
+const addTwo = function(nums){
+    return nums+2;
+}
+console.log(addTwo(4));
