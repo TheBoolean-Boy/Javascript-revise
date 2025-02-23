a2cbf2700a2d0e155e28c87879bb5f4f3f917803
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-23 11:44:53 +0000

    var violates scopes

---
+++ 03_Basics/02_scopes.js
@@ -0,0 +1,14 @@
+let a = 10;
+const  b = 20;
+var c = 30;
+
+if(true){
+    let a  = 34;
+    const b = 56;
+    var c =  67;
+    console.log(a, b, c);
+}
+console.log(a, b, c)
+
+//var violates scopes
+//local and global scopes
