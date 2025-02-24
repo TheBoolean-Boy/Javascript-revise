014d3bcc1df2d3e717ab6c62512f590a54f16f23
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-24 06:52:21 +0000

    okay

---
+++ 04_control_flow/one.js
@@ -0,0 +1,26 @@
+// const temp = 39
+// if(temp === "34"){
+//     console.log("yes");
+// }
+// else if(temp < "36"){
+//     console.log("Okay");
+// }
+// else{
+//     console.log("checked everything");
+// }
+
+const month = "kjdf"
+switch(month){
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    default:
+        console.log("Enter a valid month")
+        break;
+}
