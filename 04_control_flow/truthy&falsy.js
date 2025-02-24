c5348cf6fb5fd7b5497c61911cfe2ddf3ea32997
Saswat Rath <94009506+TheBoolean-Boy@users.noreply.github.com>
2025-02-24 07:16:18 +0000

    Nullish coalescing Operator ?? & Ternary Operator ?

--- 04_control_flow/truthy&falsy.js
+++ 04_control_flow/truthy&falsy.js
@@ -8,12 +8,26 @@
 const myObj = {}
 
 if(Object.keys(myObj).length === 0){
-    console.log("Empty Oject")
+    // console.log("Empty Oject")
 }
 
 //check for empty array
 const myArr = []
 if(myArr.length === 0){
-    console.log("Empty Array");
+    // console.log("Empty Array");
     
 }
+
+// Nullish coalescing Operator (??): null undefined
+
+let val1  = null ?? "saswat"
+// console.log(val1);
+
+val1 = null ?? undefined
+// console.log(val1);
+
+
+//Ternary Operator
+
+const val = (3>4)?3 : 2
+console.log(val);
